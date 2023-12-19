535d40efb9c316054673afc7e649cb48638299e1
lukassta <lukas.stavaris@gmail.com>
2023-12-19 19:53:13 +0200

    Added render function

--- pong.cpp
+++ pong.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+int w = 30;
+int h = 20;
+
+void render(){
+    system("clear");
+    for(int i = 0; i < h; i++){
+        for(int j = 0; j < w; j++){
+            std::cout << "..";
+        }
+        std::cout << "\n";
+    }
+}
+
 int main(){
-    
+    while(true) {
+        render();
+    }
     return 0;
 }
