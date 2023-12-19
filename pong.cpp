26357b2eb1bda8e6967e903b3e53898e56057fb4
lukassta <lukas.stavaris@gmail.com>
2023-12-19 21:17:00 +0200

    Added keyboard input with multithreading

--- pong.cpp
+++ pong.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <cmath>
+#include <thread>
+#include <chrono>
 
 int w = 30;
 int h = 20;
-char air = '.';
+int input;
+
+void input_thread()
+{
+    while(true){
+        system("stty raw");
+        input = getchar(); 
+        if(input == 27) {
+            system("stty cooked");
+            exit(0);
+        }  
+    }
+}
 
 void render(int ballX, int ballY){
+    char air = '.';
+
     system("clear");
     for(int y = 0; y < h; y++){
+        std::cout << "\r";
         for(int x = 0; x < w; x++){
             if(x == round(ballX) && y == round(ballY)) std::cout << "()";
             else std::cout << air << air;
@@ -17,6 +34,7 @@ void render(int ballX, int ballY){
 }
 
 int main(){
+    std::thread input_t(&input_thread);
     while(true) {
         render(5,5);
     }
