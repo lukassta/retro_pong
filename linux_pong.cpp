bac0cf75d248c00ab908c2b705bc280cb8fc28af
lukassta <lukas.stavaris@gmail.com>
2023-12-20 21:09:21 +0200

    Added player platform with collission

--- linux_pong.cpp
+++ linux_pong.cpp
@@ -8,25 +8,21 @@ int input;
 int w = 30;
 int h = 20;
 
-float ballX = 10;
-float ballY = 5;
+float ballX = w/2.0;
+float ballY = h/2.0;
+
+int playerH = 5;
+float playerY = 2;
+float playerSpeed = 0.05;
+
 
 int angle = 45;
-float speed = 0.01;
+float speed = 0.1;
 
 void physics_thread(){
     while(true){
         ballX+=cos(angle*3.14159/180)*speed;
         ballY+=sin(angle*3.14159/180)*speed;
-        
-        if(ballX < 0){
-            ballX = -ballX;
-            angle = angle/180*180+abs(180 - angle%180);
-        }
-        else if(w < ballX){
-            ballX = 2*w-ballX;
-            angle = angle/180*180+abs(180 - angle%180);
-        }
 
         if(ballY < 0){
             ballY = -ballY;
@@ -37,8 +33,19 @@ void physics_thread(){
             angle = 360 - angle;
         }
 
-        
+        if( 1 <= ballX && ballX <= 2 && playerY <= ballY && ballY <= playerY + playerH){
+            angle = angle/180*180+abs(180 - angle%180);
+        }
 
+        if(ballX < 0){
+            ballX = -ballX;
+            angle = angle/180*180+abs(180 - angle%180);
+        }
+        else if(w < ballX){
+            ballX = 2*w-ballX;
+            angle = angle/180*180+abs(180 - angle%180);
+        }
+       
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 }
@@ -61,7 +68,8 @@ void render(int ballXCoordinate, int ballYCoordinate){
     for(int y = 0; y < h; y++){
         std::cout << "\r";
         for(int x = 0; x < w; x++){
-            if(x == round(ballXCoordinate) && y == round(ballYCoordinate)) std::cout << "()";
+            if(x == 1 && playerY <= y && y <= playerY + playerH) std::cout << "[]";
+            else if(x == round(ballXCoordinate) && y == round(ballYCoordinate)) std::cout << "()";
             else std::cout << air << air;
         }
         std::cout << "\n";
@@ -73,6 +81,9 @@ int main(){
     std::thread physics_t(&physics_thread);
 
     while(true) {
+        if(input == 100 && playerY + playerH <= h-1) playerY += playerSpeed;
+        if(input == 97  && playerY >= 0) playerY -= playerSpeed;
+        
         render(ballX,ballY);
     }
     return 0;
